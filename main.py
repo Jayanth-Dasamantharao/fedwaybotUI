c53e6edcb655fba4a40389f04a9f3139a02c688d
dj480 <130173253+Jayanth-Dasamantharao@users.noreply.github.com>
2024-09-09 12:24:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -227,7 +227,7 @@ if __name__ == '__main__':
             display: block;
             margin-left: auto;
             margin-right: auto;
-            width: 10;
+            width: 75px;
         }
         .stChatMessage {
             font-size: 14px;
